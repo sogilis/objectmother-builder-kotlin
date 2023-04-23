55fd171bbbe8af645a48a982db25783c3e85ec9a
Jean-Baptiste Mille <jean-baptiste.mille@aselta.com>
2023-04-23 17:53:03 +0200

    Override objets with a lambda

--- src/main/kotlin/POC.kt
+++ src/main/kotlin/POC.kt
@@ -1,8 +1,9 @@
 import FormeTraits.CENTERED
+import FormeTraits.CENTERED_AND_SQUARED
 import FormeTraits.SQUARED
 
 // The class used in test
-class Forme(var x: Int, var y: Int, var width: Int, var height: Int) {
+class Forme(var x: Int, var y: Int, var width: Int, var height: Int, var label: String?) {
     fun fill() {}
     fun empty() {}
 
@@ -16,8 +17,11 @@ fun test() {
     Forme.build(SQUARED)
     Forme.build(SQUARED, CENTERED)
     Forme.build(SQUARED, CENTERED, y = 10)
-    Forme.build().apply { fill() }
-    Forme.build(SQUARED, CENTERED, y = 10).apply { fill() }
+    Forme.build { fill() }
+    Forme.build(CENTERED_AND_SQUARED, y = 10) {
+        empty()
+        label = "my forme"
+    }
 }
 
 // Factory_bot like builder
@@ -29,7 +33,8 @@ private fun Forme.Companion.build(
     width: Int = 100,
     height: Int = 100,
     label: String = "width = $width", // Dynamic attribute based on other attribute
-) = traits.collect(Forme(x, y, width, height))
+    override: Forme.() -> Unit = {}
+) = traits.collect(Forme(x, y, width, height, label)).apply(override)
 
 object FormeTraits {
     val SQUARED: Trait<Forme> = { it.apply { height = width } }
